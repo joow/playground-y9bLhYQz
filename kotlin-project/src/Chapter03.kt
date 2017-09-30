b3f8bcfc7974cc874720af60d851771badae8353
Benoît Giraudou <bgiraudou@gmail.com>
2017-09-30 16:06:55 +0200

    Define empty functions for chapter 3 exercises

---
+++ kotlin-project/src/Chapter03.kt
@@ -0,0 +1,19 @@
+import java.awt.Point
+
+fun add(a: Int, b: Int): Int = TODO()
+
+fun printTheAnswer(): Unit = TODO()
+
+fun strEq(s1: String, s2: String): Boolean = TODO()
+
+fun strEq(s1: String, s2: String, ignoreCase: Boolean): Boolean = TODO()
+
+fun isEven(n: Int): Boolean = TODO()
+
+infix fun Int.inc(n: Int): Int = TODO()
+
+fun sum(vararg xs: Int): Int = TODO()
+
+operator fun Point.plus(other: Point): Point = TODO()
+
+fun freq(s: String, c: Char): Int = TODO()
